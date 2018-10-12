c83ee41d09d1a2c64bcd578a10e69d41972e39a3
mohitacharya4 <mohitacharya4@gmail.com>
2018-10-12 12:45:20 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using GettingQuestionOptionAndType;
+
+
+namespace GettingQuestionOptionAndType
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string downloadedDocument = "{\n \"pages\": [\n  {\n   \"name\": \"Annual Company Questionnaire\",\n   \"elements\": [\n    {\n     \"type\": \"text\",\n     \"name\": \"question1\"\n    },\n    {\n     \"type\": \"checkbox\",\n     \"name\": \"question2\",\n     \"choices\": [\n      \"item1\",\n      \"item2\",\n      \"item3\"\n     ]\n    },\n    {\n     \"type\": \"radiogroup\",\n     \"name\": \"question3\",\n     \"choices\": [\n      \"item1\",\n      \"item2\",\n      \"item3\"\n     ]\n    },\n    {\n     \"type\": \"dropdown\",\n     \"name\": \"question4\",\n     \"choices\": [\n      \"item1\",\n      \"item2\",\n      \"item3\"\n     ]\n    }\n   ],\n   \"title\": \"Annual Company Questionnaire\"\n  },\n  {\n   \"name\": \"Annual Plan Questionnaire\",\n   \"title\": \"Annual Plan Questionnaire\"\n  },\n  {\n   \"name\": \"Annual Form 5500 Questionnaire\",\n   \"title\": \"Annual Form 5500 Questionnaire\"\n  }\n ]\n}";
+            var x = JsonConvert.DeserializeObject<RootObject>(downloadedDocument);
+            Console.WriteLine(x);
+
+
+        }
+    }
+}
---
+++ survey.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingQuestionOptionAndType
+{
+    public class Element
+    {
+        public string type { get; set; }
+        public string name { get; set; }
+        public List<string> choices { get; set; }
+    }
+
+    public class Page
+    {
+        public string name { get; set; }
+        public List<Element> elements { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<Page> pages { get; set; }
+
+    }
+}
